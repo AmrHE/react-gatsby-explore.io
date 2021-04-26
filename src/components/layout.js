93b0859b62007613dbd9d87f9a68ca3d94f63440
AmrHE <amr.hassan.emam@gmail.com>
2021-04-26 17:47:11 +0200

    import GlobalStyle

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,11 +1,13 @@
 import * as React from "react"
-import Header from "./header"
+import Header from "./Header"
+import { GlobalStlye } from "./styles/GlobalStyles"
 
 
 const Layout = ({ children }) => {
 
   return (
     <>
+      <GlobalStlye />
       <Header/>
         <main>{children}</main>
     </>
