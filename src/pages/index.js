01d4c81d9f9c93aa723614d619496336116f53f8
AmrHE <amr.hassan.emam@gmail.com>
2021-04-27 01:59:48 +0200

    import Trips Component

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,11 +2,13 @@ import React from "react"
 import Hero from "../components/Hero"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+import Trips from "../components/Trips"
 
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
     <Hero />
+    <Trips heading="Our Favorite Destinations"/>
   </Layout>
 )
 
