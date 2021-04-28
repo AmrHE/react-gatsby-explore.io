770784794745f6e2ef6cee1770e71fc0581993e4
AmrHE <amr.hassan.emam@gmail.com>
2021-04-28 06:43:08 +0200

    import new components

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,13 +2,17 @@ import React from "react"
 import Hero from "../components/Hero"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+import Stats from "../components/Stats"
+import Testimonials from "../components/Testimonials"
 import Trips from "../components/Trips"
 
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
     <Hero />
-    <Trips heading="Our Favorite Destinations"/>
+    <Trips heading="Our Favorite Destinations" />
+    <Testimonials />
+    <Stats />
   </Layout>
 )
 
