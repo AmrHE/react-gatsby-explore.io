1d151666b3229a4d8c016ca6226fc1055b21042b
AmrHE <amr.hassan.emam@gmail.com>
2021-04-30 02:29:29 +0200

    build careers page

---
+++ src/pages/careers.js
@@ -0,0 +1,13 @@
+import React from "react"
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+import Stats from "../components/Stats"
+
+const CareersPage = () => (
+  <Layout>
+    <Seo title="Careers" />
+    <Stats />
+  </Layout>
+)
+
+export default CareersPage
