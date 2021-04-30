172880989e8134eccd91b58c8403946945230487
AmrHE <amr.hassan.emam@gmail.com>
2021-04-30 02:29:58 +0200

    add contact us page

---
+++ src/pages/contact.js
@@ -0,0 +1,13 @@
+import React from "react"
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+import Email from "../components/Email"
+
+const ContactPage = () => (
+  <Layout>
+    <Seo title="Contact Us" />
+    <Email />
+  </Layout>
+)
+
+export default ContactPage
