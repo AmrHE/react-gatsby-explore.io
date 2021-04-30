a5ad564d90ae86b416cf9e540d11344d48cff508
AmrHE <amr.hassan.emam@gmail.com>
2021-04-30 02:28:15 +0200

    edit layout component

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,15 +1,22 @@
-import * as React from "react"
+import React, { useState } from "react"
+import Dropdown from "./Dropdown"
+import Footer from "./Footer"
 import Header from "./Header"
 import { GlobalStlye } from "./styles/GlobalStyles"
 
-
 const Layout = ({ children }) => {
+  const [isOpen, setIsOpen] = useState(false)
 
+  const toggle = () => {
+    setIsOpen(!isOpen)
+  }
   return (
     <>
       <GlobalStlye />
-      <Header/>
-        <main>{children}</main>
+      <Dropdown isOpen={isOpen} toggle={toggle} />
+      <Header toggle={toggle} />
+      <main>{children}</main>
+      <Footer />
     </>
   )
 }
